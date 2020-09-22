5444c296119f959ff4e555e03dc7b2c0e8bb75d5
dubis78 <43077233+dubis78@users.noreply.github.com>
2020-09-22 14:11:40 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set("port", 4001);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//función middleware para archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(__dirname);
+
+
+
+//Adicionamos la segunda página 
+app.use('/statics', (req,res)=>{
+  res.sendFile(path.join(__dirname, './public/home.html'));
+});
+
+app.listen(app.get("port"), ()=>{
+    console.log(`Server on port 4001`);
+  })
